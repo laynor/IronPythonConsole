1b60704d80f713f3732856f2fb1f050196d07136
unknown <M6MACHUN@M601Q027.cznet.zeiss.org>
2015-06-30 09:37:43 -0700

    Working version of console. Currently working on adding preloaded scripts so modules are imported/added before user begins

---
+++ IronPythonConsole/IronPythonConsole.py
@@ -0,0 +1,12 @@
+# Start up script for IronPythonConsole
+import os
+#import sys
+##import rypc
+
+# Set properties for proper behaviour with PyDoc
+os.environ['TERM'] = 'dumb'
+
+# adds python27
+#sys.path.append(r"c:\python27\lib\site-packages")
+
+#c = rypc.classic.connect("localhost")
